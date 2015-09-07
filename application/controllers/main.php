d243d80e5286f5b6da84efbc0b98999dab6ff0da
OA Wu <comdan66@gmail.com>
2015-09-07 11:34:29 +0800

    Update composer, and sample mail gun.

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -12,11 +12,13 @@ class Main extends Site_controller {
   }
 
   public function index () {
+    $msg = 'Hi OA,<br/><br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;非常感謝您的加入，現在只差最後一個步驟了，請點擊下列網址以啟動您的帳號吧！<br/><br/>驗證網址: <br/>';
+
     $this->load->library ('OaMailGun');
     $mail = new OaMailGun ();
     $result = $mail->sendMessage (array (
               'from' => Cfg::setting ('mail_gun', 'user', 'system', 'name') . ' <' . Cfg::setting ('mail_gun', 'user', 'system', 'email') . '>',
-              'to' => $temp->name . ' <' . $temp->email . '>',
+              'to' => 'OA' . ' <' . 'comdan66@gmail.com' . '>',
               'subject' => Cfg::setting ('mail_gun', 'user', 'system', 'subject'),
               'html' => $msg
             ));
