f7fb283ed8a3840692f571ba9d0fb5f665dd3954
OA Wu <comdan66@gmail.com>
2015-09-07 11:03:15 +0800

    Add admin login cell.

---
+++ application/cell/controllers/admin_login_cell.php
@@ -0,0 +1,26 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 OA Wu Design
+ */
+
+class Admin_login_cell extends Cell_Controller {
+
+  /* render_cell ('admin_login_cell', 'loading', var1, ..); */
+  // public function loading () {
+  //   return array ('time' => 60 * 60, 'key' => null);
+  // }
+  public function loading () {
+    return $this->load_view ();
+  }
+
+  /* render_cell ('admin_login_cell', 'footer', var1, ..); */
+  // public function footer () {
+  //   return array ('time' => 60 * 60, 'key' => null);
+  // }
+  public function footer () {
+    return $this->load_view (array (
+      ));
+  }
+}
