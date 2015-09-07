2b61ca863d8e384646aeae9bca02d37fbd8b6c0b
OA Wu <comdan66@gmail.com>
2015-09-07 16:53:51 +0800

    Fix right menu bug.

--- application/cell/views/admin_cell/nav/content.php
+++ application/cell/views/admin_cell/nav/content.php
@@ -12,12 +12,14 @@
           <a class='icon-menu'>分享</a>
     <?php if (!User::current ()) { ?>
             <a href='<?php echo Fb::loginUrl ('platform', 'fb_sign_in');?>' class='top_line icon-link-external'>登入</a>
-    <?php } else { ?>
-              <a href='<?php echo Fb::logoutUrl ('platform', 'sign_out');?>' class='top_line icon-menu'>登出</a>
-        <?php if (in_array ('admin', User::current ()->role_names ())) { ?>
-                <a class='icon-menu' href='<?php echo base_url ('admin');?>'>管理</a>
-        <?php }?>
-    <?php }?>
+    <?php } else {
+            if (in_array ('admin', User::current ()->role_names ())) { ?>
+              <a class='icon-menu top_line' href='<?php echo base_url ('admin');?>'>管理</a>
+              <a class='icon-menu' href='<?php echo Fb::logoutUrl ('platform', 'sign_out');?>'>登出</a>
+      <?php } else { ?>
+              <a class='icon-menu top_line' href='<?php echo Fb::logoutUrl ('platform', 'sign_out');?>'>登出</a>
+      <?php }
+          }?>
         </div>
       </div>
 
