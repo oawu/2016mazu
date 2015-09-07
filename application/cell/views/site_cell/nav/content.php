234e042bcd57c4e1d007fd930c7eb9734f35d4bd
OA Wu <comdan66@gmail.com>
2015-09-07 21:13:36 +0800

    Add roles for admin.

--- application/cell/views/site_cell/nav/content.php
+++ application/cell/views/site_cell/nav/content.php
@@ -8,12 +8,12 @@
 
       <div class='option icon-more white'>
         <div class='cover'></div>
-        <div class='menu i<?php echo 1 + (User::current () ? in_array ('admin', User::current ()->role_names ()) ? 2 : 1 : 1);?>'>
+        <div class='menu i<?php echo 1 + (User::current () ? array_intersect (Cfg::setting ('admin', 'roles'), User::current ()->role_names ()) ? 2 : 1 : 1);?>'>
           <a class='icon-menu'>分享</a>
     <?php if (!User::current ()) { ?>
             <a href='<?php echo Fb::loginUrl ('platform', 'fb_sign_in');?>' class='top_line icon-link-external'>登入</a>
     <?php } else {
-            if (in_array ('admin', User::current ()->role_names ())) { ?>
+            if (array_intersect (Cfg::setting ('admin', 'roles'), User::current ()->role_names ())) { ?>
               <a class='icon-menu top_line' href='<?php echo base_url ('admin');?>'>管理</a>
               <a class='icon-menu' href='<?php echo Fb::logoutUrl ('platform', 'sign_out');?>'>登出</a>
       <?php } else { ?>
--- application/config/setting/admin.php
+++ application/config/setting/admin.php
@@ -8,3 +8,5 @@
 $admin['main']['title'] = '後台管理';
 $admin['main']['footer']['title'] = '北港迎媽祖 © 2015';
 $admin['main']['footer']['description'] = '如有相關問題歡迎與<a href="https://www.facebook.com/comdan66" target="_blank">作者</a>討論。';
+
+$admin['roles'] = array ('root', 'admin');
