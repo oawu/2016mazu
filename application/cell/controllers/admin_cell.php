917f6e933f71a49d4660f710369ffe801e6288be
OA Wu <comdan66@gmail.com>
2015-09-10 12:06:52 +0800

    Fix admin left menu item icon.

--- application/cell/controllers/admin_cell.php
+++ application/cell/controllers/admin_cell.php
@@ -18,9 +18,9 @@ class Admin_cell extends Cell_Controller {
     $item_lists = array (
       '權限' => array (
           array ('name' => '首頁', 'href' => base_url ('admin'), 'icon' => 'icon-home', 'target' => '_self', 'visible' => true, 'active' => ($class == 'maind') && ($method == 'index')),
-          array ('name' => '角色設定', 'href' => base_url ('admin', 'roles'), 'icon' => 'icon-fire', 'target' => '_self', 'visible' => true, 'active' => ($class == 'roles')),
-          array ('name' => '項目設定', 'href' => base_url ('admin', 'menus'), 'icon' => 'icon-fire', 'target' => '_self', 'visible' => true, 'active' => ($class == 'menus')),
-          array ('name' => '使用者設定', 'href' => base_url ('admin', 'users'), 'icon' => 'icon-fire', 'target' => '_self', 'visible' => true, 'active' => ($class == 'users')),
+          array ('name' => '角色設定', 'href' => base_url ('admin', 'roles'), 'icon' => 'icon-user', 'target' => '_self', 'visible' => true, 'active' => ($class == 'roles')),
+          array ('name' => '項目設定', 'href' => base_url ('admin', 'menus'), 'icon' => 'icon-list2', 'target' => '_self', 'visible' => true, 'active' => ($class == 'menus')),
+          array ('name' => '使用者設定', 'href' => base_url ('admin', 'users'), 'icon' => 'icon-user2', 'target' => '_self', 'visible' => true, 'active' => ($class == 'users')),
         ),
       '文章上搞'=> array (
         ),
