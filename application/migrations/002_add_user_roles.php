946997e3a148444df75b0a541d4034c7e20680c0
OA Wu <comdan66@gmail.com>
2015-09-15 18:20:25 +0800

    Restruct role migration.

--- application/migrations/002_add_user_roles.php
+++ application/migrations/002_add_user_roles.php
@@ -5,27 +5,26 @@
  * @copyright   Copyright (c) 2015 OA Wu Design
  */
 
-class Migration_Add_menu_roles extends CI_Migration {
+class Migration_Add_user_roles extends CI_Migration {
   public function up () {
     $this->db->query (
-      "CREATE TABLE `menu_roles` (
+      "CREATE TABLE `user_roles` (
         `id` int(11) NOT NULL AUTO_INCREMENT,
-        `menu_id` int(11) NOT NULL COMMENT 'Menu ID',
-        `role_id` int(11) NOT NULL COMMENT 'Role ID',
+        `user_id` int(11) NOT NULL COMMENT 'User ID',
+        `role` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT '角色',
         `updated_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '更新時間',
         `created_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '新增時間',
         PRIMARY KEY (`id`),
-        KEY `menu_id_index` (`menu_id`),
-        KEY `role_id_index` (`role_id`),
-        UNIQUE KEY `menu_id_role_id_unique` (`menu_id`, `role_id`),
-        FOREIGN KEY (`menu_id`) REFERENCES `menus` (`id`) ON DELETE CASCADE,
-        FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE
+        KEY `user_id_index` (`user_id`),
+        KEY `role_index` (`role`),
+        UNIQUE KEY `user_id_role_unique` (`user_id`, `role`),
+        FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
     );
   }
   public function down () {
     $this->db->query (
-      "DROP TABLE `menu_roles`;"
+      "DROP TABLE `user_roles`;"
     );
   }
 }
