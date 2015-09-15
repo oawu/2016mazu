aa2f550310766589488b38105269baf1703ba1e4
OA Wu <comdan66@gmail.com>
2015-09-15 18:21:07 +0800

    Restruct role model.

--- application/models/User.php
+++ application/models/User.php
@@ -13,31 +13,15 @@ class User extends OaModel {
   );
 
   static $has_many = array (
-    array ('user_roles', 'class_name' => 'UserRole')
+    array ('roles', 'class_name' => 'UserRole')
   );
 
   static $belongs_to = array (
   );
-
   private static $current = '';
-  private $roles = null;
 
   public function __construct ($attributes = array (), $guard_attributes = true, $instantiating_via_find = false, $new_record = true) {
     parent::__construct ($attributes, $guard_attributes, $instantiating_via_find, $new_record);
-  }
-
-  public function roles () {
-    if ($this->roles !== null)
-      return $this->roles;
-
-    if (!($role_ids = column_array ($this->user_roles, 'role_id')))
-      return $this->roles = array ();
-
-    return $this->roles = Role::find ('all', array ('conditions' => array ('id IN (?)', $role_ids)));
-  }
-
-  public function role_names () {
-    return column_array ($this->roles (), 'name');
   }
 
   public static function current () {
@@ -50,6 +34,10 @@ class User extends OaModel {
         return self::$current = null;
   }
 
+  public function roles () {
+    return column_array ($this->roles, 'role');
+  }
+
   public function avatar ($w = 100, $h = 100) {
     $size = array ();
     array_push ($size, isset ($w) && $w ? 'width=' . $w : '');
@@ -57,4 +45,24 @@ class User extends OaModel {
 
     return 'https://graph.facebook.com/' . $this->uid . '/picture' . (($size = implode ('&', array_filter ($size))) ? '?' . $size : '');
   }
+
+
+
+
+
+  // private $roles = null;
+
+
+  // public function roles () {
+  //   if ($this->roles !== null)
+  //     return $this->roles;
+
+  //   if (!($role_ids = column_array ($this->user_roles, 'role_id')))
+  //     return $this->roles = array ();
+
+  //   return $this->roles = Role::find ('all', array ('conditions' => array ('id IN (?)', $role_ids)));
+  // }
+
+
+
 }
