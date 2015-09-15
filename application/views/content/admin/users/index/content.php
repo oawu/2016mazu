1e619b862764de7864473f24328afd520f15af53
OA Wu <comdan66@gmail.com>
2015-09-15 21:26:02 +0800

    Fix layout

--- application/views/content/admin/users/index/content.php
+++ application/views/content/admin/users/index/content.php
@@ -1,7 +1,7 @@
 <form action='<?php echo base_url ('admin', 'roles', $role->id, 'users');?>' method='get' class="search<?php echo $has_search ? ' show' : '';?>">
   <div class='l i3'>
     <input type='text' name='id' value='<?php echo @$columns['id'];?>' placeholder='請輸入 ID..' />
-    <input type='text' name='name' value='<?php echo @$columns['name'];?>' placeholder='請輸入 標題..' />
+    <input type='text' name='name' value='<?php echo @$columns['name'];?>' placeholder='請輸入 名稱..' />
     <input type='text' name='email' value='<?php echo @$columns['email'];?>' placeholder='請輸入 信箱..' />
   </div>
   <button type='submit'>尋找</button>
@@ -14,7 +14,7 @@
         foreach ($users as $user) { ?>
           <tr>
             <td data-title='ID' width='80'><?php echo $user->id;?></td>
-            <td data-title='頭像' width='40'><?php echo img ($user->avatar (30, 30));?></td>
+            <td data-title='頭像' width='40'><?php echo img ($user->avatar (30, 30), false, 'class="avatar"');?></td>
             <td data-title='名稱'><?php echo $user->name;?></td>
             <td data-title='信箱' width='250'><?php echo $user->email;?></td>
             <td data-title='角色' width='150'><?php echo implode ('', array_map (function ($role) { return '<a class="role">' . $role . '</a>';}, $user->roles ()));?></td>
