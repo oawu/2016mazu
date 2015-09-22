ae3d5f655d6e973cdd4da23a4b1f8adce343bf39
OA Wu <comdan66@gmail.com>
2015-09-22 09:41:19 +0800

    Add menu config.

---
+++ application/config/setting/menu.php
@@ -0,0 +1,45 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 OA Wu Design
+ */
+
+$menu['site'] = array (
+    '朝天宮' => array (),
+    '三月十九' => array (
+        '陣頭路關' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (),
+        '藝閣路關' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (),
+        '陣頭遶境地圖' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (
+            '三月十九' => array ('href' => '', 'class' => '', 'method' => ''),
+            '三月二十' => array ('href' => '', 'class' => '', 'method' => ''),
+          )),
+        '藝閣遶境地圖' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (
+            '三月十九' => array ('href' => '', 'class' => '', 'method' => ''),
+            '三月二十' => array ('href' => '', 'class' => '', 'method' => ''),
+            '三月廿一' => array ('href' => '', 'class' => '', 'method' => ''),
+            '三月廿二' => array ('href' => '', 'class' => '', 'method' => ''),
+            '三月廿三' => array ('href' => '', 'class' => '', 'method' => ''),
+          ),
+        '2015 回顧' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array ()),
+      ),
+    '百年藝陣' => array (
+        '駕前陣頭' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (),
+        '地方陣頭' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (),
+        '其他介紹' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (),
+      ),
+    '文化交流' => array (
+        '白沙屯 2015 路線' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (),
+      ),
+    '美食旅遊' => array (
+        '美食地圖' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (),
+        '在地小吃' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (),
+        '名勝景點' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (),
+        '交通資訊' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (),
+      ),
+    '吾鄉笨港' => array (
+        '老照片說故事' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (),
+        '耆老回憶' => array ('icon' => '', 'href' => '', 'class' => '', 'method' => '', 'target' => '_self', 'tags' => array (),
+      ),
+    '其他資源' => array (),
+  );
