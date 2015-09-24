1144d04ae663ad5e3454d43ba038c6b4053c627b
OA Wu <comdan66@gmail.com>
2015-09-24 17:33:01 +0800

    Create dintaos model.

---
+++ application/models/Dintao.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 OA Wu Design
+ */
+
+class Dintao extends OaModel {
+
+  static $table_name = 'Dintaos';
+
+  static $has_one = array (
+  );
+
+  static $has_many = array (
+  );
+
+  static $belongs_to = array (
+  );
+
+  const TYPE_OFFICIAL = 1;
+  const TYPE_LOCAL    = 2;
+  const TYPE_OTHER    = 3;
+  static $types = array (
+      self::TYPE_OFFICIAL => '駕前陣頭',
+      self::TYPE_LOCAL => '地方陣頭',
+      self::TYPE_OTHER => '其他介紹',
+    );
+
+  public function __construct ($attributes = array (), $guard_attributes = true, $instantiating_via_find = false, $new_record = true) {
+    parent::__construct ($attributes, $guard_attributes, $instantiating_via_find, $new_record);
+
+    OrmImageUploader::bind ('cover', 'DintaoCoverImageUploader');
+  }
+}
