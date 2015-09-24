9e7ec36fb4dc5ac27051549855ce4b9771552c24
OA Wu <comdan66@gmail.com>
2015-09-24 17:35:39 +0800

    Add admin js feature.

--- application/views/frame/admin/frame.js
+++ application/views/frame/admin/frame.js
@@ -6,4 +6,22 @@
 $(function () {
   $('.timeago').timeago ();
   $('.imgLiquid_center').imgLiquid ({verticalAlign: 'center'});
+  autosize ($('textarea.autosize'));
+
+  CKEDITOR.on ('dialogDefinition', function (ev) {
+
+    var dialogName = ev.data.name;
+    var dialogDefinition = ev.data.definition;
+
+    if (dialogName == 'link') {
+      var infoTab = dialogDefinition.getContents ('info');
+      infoTab.remove ('linkType');
+      dialogDefinition.getContents ('target').get ('linkTargetType')['default'] = '_blank';
+    }
+  });
+
+  $('textarea.ckeditor').ckeditor ({
+    filebrowserUploadUrl: 'asd',
+    height: 400,
+  });
 });
