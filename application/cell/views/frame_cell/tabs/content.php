e07658c91b6cfb7ec924a025165722ccf6a2d59b
OA Wu <comdan66@gmail.com>
2015-09-24 15:18:35 +0800

    Fix tabs href.

--- application/cell/views/frame_cell/tabs/content.php
+++ application/cell/views/frame_cell/tabs/content.php
@@ -7,7 +7,7 @@
   <div>
     <div>
 <?php foreach ($tabs as $text => $tab) { ?>
-        <a href=''<?php echo ($index !== null) && isset ($tab['index']) && ($tab['index'] == $index) ? " class='a'": '';?>><?php echo $text;?></a>
+        <a<?php echo isset ($tab['href']) ? " href='" . $tab['href'] . "'" : '';?><?php echo ($index !== null) && isset ($tab['index']) && ($tab['index'] == $index) ? " class='a'": '';?>><?php echo $text;?></a>
 <?php } ?>
     </div>
   </div>
