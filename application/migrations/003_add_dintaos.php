402a7042b643d43bfeb46a48f746a825eaaeadb5
OA Wu <comdan66@gmail.com>
2015-09-25 14:46:06 +0800

    Fix dintao DB schema.

--- application/migrations/003_add_dintaos.php
+++ application/migrations/003_add_dintaos.php
@@ -21,6 +21,7 @@ class Migration_Add_dintaos extends CI_Migration {
         `updated_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '更新時間',
         `created_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '新增時間',
         PRIMARY KEY (`id`),
+        KEY `type_index` (`type`),
         KEY `user_id_index` (`user_id`),
         FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
