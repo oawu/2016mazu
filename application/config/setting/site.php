85f84583d23b6028c51635ba501a68bbbb3201c5
OA Wu <comdan66@gmail.com>
2015-10-01 16:25:05 +0800

    Add config for site.

--- application/config/setting/site.php
+++ application/config/setting/site.php
@@ -8,3 +8,5 @@
 $site['main']['title'] = '北港迎媽祖';
 $site['main']['footer']['title'] = '北港迎媽祖 © 2015';
 $site['main']['footer']['description'] = '如有相關問題歡迎與<a href="https://www.facebook.com/comdan66" target="_blank">作者</a>討論。';
+$site['main']['keywords'] = '北港迎媽祖 © 2015';
+$site['main']['description'] = '北港迎媽祖 © 2015';
