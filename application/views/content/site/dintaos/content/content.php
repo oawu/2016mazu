f2aec21833d61ec122aa26b5392eeaae6c7bb996
OA Wu <comdan66@gmail.com>
2015-10-02 23:32:40 +0800

    Temp dintao content page.

--- application/views/content/site/dintaos/content/content.php
+++ application/views/content/site/dintaos/content/content.php
@@ -1,7 +1,20 @@
-<h2>
-  <?php echo $dintao->title;?>
-</h2>
+<div class='t'>
+  <h2>
+    <?php echo $dintao->title;?>
+  </h2>
+  <div class="fb-like" data-href="<?php echo base_url ('dintao', $dintao->id);?>" data-send="false" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
+</div>
 
 <article>
   <?php echo $dintao->content;?>
 </article>
+
+<div class='s'>
+  <div>相關參考：</div>
+  <ul>
+    <li><a href='' target='_blank'>笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港笨港到北港</a></li>
+    <li><a href='' target='_blank'>asdadasdsad</a></li>
+    <li><a href='' target='_blank'>asdadsad</a></li>
+    <li><a href='' target='_blank'>asddaadsad</a></li>
+  </ul>
+</div>
