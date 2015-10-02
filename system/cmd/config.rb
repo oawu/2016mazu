d68b296a0cd3dd7167b30c978e6250023678a0bb
OA Wu <comdan66@gmail.com>
2015-10-03 01:16:34 +0800

    Rebuild css.

--- system/cmd/config.rb
+++ system/cmd/config.rb
@@ -20,8 +20,8 @@ fonts_path = "../../resource/font/"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
-# output_style = :compressed
+# output_style = :expanded
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
