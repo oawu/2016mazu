10090286297193d9aac30aa1d6d75ca1e6b2bfd4
OA Wu <comdan66@gmail.com>
2015-10-05 10:56:44 +0800

    Add picture migrations.

---
+++ application/migrations/006_add_pictures.php
@@ -0,0 +1,29 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 OA Wu Design
+ */
+
+class Migration_Add_pictures extends CI_Migration {
+  public function up () {
+    $this->db->query (
+      "CREATE TABLE `pictures` (
+        `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+
+        `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '標題',
+        `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '名稱',
+        `keywords` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'SEO 關鍵字',
+
+        `updated_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '更新時間',
+        `created_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '新增時間',
+        PRIMARY KEY (`id`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
+    );
+  }
+  public function down () {
+    $this->db->query (
+      "DROP TABLE `pictures`;"
+    );
+  }
+}
