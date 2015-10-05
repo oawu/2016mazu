617f9612bef3d284095526602970c05d7f2eb667
OA Wu <comdan66@gmail.com>
2015-10-05 11:47:01 +0800

    Fix Picture Tags DB schema.

--- application/migrations/007_add_picture_tags.php
+++ application/migrations/007_add_picture_tags.php
@@ -16,6 +16,10 @@ class Migration_Add_picture_tags extends CI_Migration {
         `keywords` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'SEO 關鍵字',
         `sort` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '排列順序，上至下 DESC',
 
+        `cover_color_r` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'RGB Red',
+        `cover_color_g` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'RGB Green',
+        `cover_color_b` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'RGB Blue',
+
         `updated_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '更新時間',
         `created_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '新增時間',
         PRIMARY KEY (`id`)
