6d3594216de2aeaa84e750a28c20afae8712522f
OA Wu <comdan66@gmail.com>
2015-10-06 14:44:18 +0800

    Fix coding style.

--- application/controllers/dintaos.php
+++ application/controllers/dintaos.php
@@ -11,34 +11,6 @@ class Dintaos extends Site_controller {
     parent::__construct ();
   }
 
-  public function content ($id = 0) {
-    if (!($id && ($dintao = Dintao::find_by_id ($id))))
-      return redirect_message (array ('dintaos', 'official'), array (
-          '_flash_message' => '無此文章！'
-        ));
-
-    $method = $dintao->type != Dintao::TYPE_OTHER ? $dintao->type != Dintao::TYPE_LOCAL ? 'official' : 'local' : 'other';
-    
-    if (!preg_match ('/^data:/', $og_img = $dintao->cover->url ('1200x630c')))
-      $this->add_meta (array ('property' => 'og:image', 'content' => $og_img, 'alt' => $dintao->title . ' - ' . Cfg::setting ('site', 'main', 'title')))
-           ->add_meta (array ('property' => 'og:image:type', 'content' => 'image/' . pathinfo ($og_img, PATHINFO_EXTENSION)))
-           ->add_meta (array ('property' => 'og:image:width', 'content' => '1200'))
-           ->add_meta (array ('property' => 'og:image:height', 'content' => '630'));
-
-    return $this->set_title ($dintao->title . ' - ' . Cfg::setting ('site', 'main', 'title'))
-                ->add_subtitle ($dintao->title)
-                ->set_back_link (base_url ($this->get_class (), $method))
-                ->add_meta (array ('name' => 'keywords', 'content' => implode (',', $dintao->keywords ())))
-                ->add_meta (array ('name' => 'description', 'content' => $dintao->mini_content ()))
-                ->add_meta (array ('property' => 'og:title', 'content' => $dintao->title))
-                ->add_meta (array ('property' => 'og:description', 'content' => $dintao->mini_content ()))
-
-                ->load_view (array (
-                    'method' => $method,
-                    'dintao' => $dintao
-                  ), false);
-  }
-
   public function index ($index = Dintao::TYPE_OTHER, $offset = 0) {
     $columns = array ('title' => 'string', 'content' => 'string', 'keywords' => 'string');
     $configs = array ($this->get_class (), Dintao::$type_engs[$index], '%s');
@@ -65,5 +37,33 @@ class Dintaos extends Site_controller {
                     'dintaos' => $dintaos,
                     'pagination' => $pagination
                   ));
+  }
+
+  public function content ($id = 0) {
+    if (!($id && ($dintao = Dintao::find_by_id ($id))))
+      return redirect_message (array ('dintaos', 'official'), array (
+          '_flash_message' => '無此文章！'
+        ));
+
+    $method = $dintao->type != Dintao::TYPE_OTHER ? $dintao->type != Dintao::TYPE_LOCAL ? 'official' : 'local' : 'other';
+    
+    if (!preg_match ('/^data:/', $og_img = $dintao->cover->url ('1200x630c')))
+      $this->add_meta (array ('property' => 'og:image', 'content' => $og_img, 'alt' => $dintao->title . ' - ' . Cfg::setting ('site', 'main', 'title')))
+           ->add_meta (array ('property' => 'og:image:type', 'content' => 'image/' . pathinfo ($og_img, PATHINFO_EXTENSION)))
+           ->add_meta (array ('property' => 'og:image:width', 'content' => '1200'))
+           ->add_meta (array ('property' => 'og:image:height', 'content' => '630'));
+
+    return $this->set_title ($dintao->title . ' - ' . Cfg::setting ('site', 'main', 'title'))
+                ->add_subtitle ($dintao->title)
+                ->set_back_link (base_url ($this->get_class (), $method))
+                ->add_meta (array ('name' => 'keywords', 'content' => implode (',', $dintao->keywords ())))
+                ->add_meta (array ('name' => 'description', 'content' => $dintao->mini_content ()))
+                ->add_meta (array ('property' => 'og:title', 'content' => $dintao->title))
+                ->add_meta (array ('property' => 'og:description', 'content' => $dintao->mini_content ()))
+
+                ->load_view (array (
+                    'method' => $method,
+                    'dintao' => $dintao
+                  ), false);
   }
 }
