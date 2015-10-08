5bed062168639b727a9061a233cc197e09dcf1f7
OA Wu <comdan66@gmail.com>
2015-10-08 16:16:39 +0800

    Fix pictures no data layout.

--- application/views/content/site/pictures/old/content.php
+++ application/views/content/site/pictures/old/content.php
@@ -1,11 +1,16 @@
 <div class='as'>
 <?php
-  foreach ($pictures as $picture) { ?>
-    <figure class='i_c' title='<?php echo $picture->title;?>' data-description='<?php echo $picture->mini_description (0);?>' href='<?php echo $picture->name->url ();?>' data-50x50c='<?php echo $picture->name->url ('50x50c');?>' data-fancybox-group='i'>
-      <img src='<?php echo $picture->name->url ('300w');?>' alt='<?php echo $picture->title;?>' />
-      <figcaption><?php echo $picture->title;?></figcaption>
-      <a href='<?php echo base_url ('pictures', $picture->id . '-' . urlencode ($picture->title));?>' target='_blank'><?php echo $picture->title;?></a>
-    </figure>
+  if ($pictures) {
+    foreach ($pictures as $picture) { ?>
+      <figure class='i_c' title='<?php echo $picture->title;?>' data-description='<?php echo $picture->mini_description (0);?>' href='<?php echo $picture->name->url ();?>' data-50x50c='<?php echo $picture->name->url ('50x50c');?>' data-fancybox-group='i'>
+        <img src='<?php echo $picture->name->url ('300w');?>' alt='<?php echo $picture->title;?>' />
+        <figcaption><?php echo $picture->title;?></figcaption>
+        <a href='<?php echo base_url ('pictures', $picture->id . '-' . urlencode ($picture->title));?>' target='_blank'><?php echo $picture->title;?></a>
+      </figure>
+<?php
+    }
+  } else { ?>
+    <div>目前尚未有任何的資料。</div>
 <?php
   }?>
 </div>
