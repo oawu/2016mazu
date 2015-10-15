ac821ccf4a6d89387b79add2391f20f3698e257e
OA Wu <comdan66@gmail.com>
2015-10-15 11:48:09 +0800

    Merge pv to ajax controller.

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -43,4 +43,41 @@ class Ajax extends Site_controller {
 
     return $this->output_json (array ('status' => true, 'content' => $content));
   }
+
+  public function pv_dintao () {
+    if (!$this->is_ajax ())
+      return show_404 ();
+
+    if (!(($id = OAInput::post ('id')) && ($dintao = Dintao::find_by_id ($id, array ('select' => 'id, pv')))))
+      return $this->output_json (array ('status' => false));
+
+    $dintao->pv += 1;
+
+    $update = Dintao::transaction (function () use ($dintao) {
+      return $dintao->save ();
+    });
+
+    if (!$update)
+      return $this->output_json (array ('status' => false));
+
+    return $this->output_json (array ('status' => true, 'pv' => $dintao->pv));
+  }
+  public function pv_picture () {
+    if (!$this->is_ajax ())
+      return show_404 ();
+
+    if (!(($id = OAInput::post ('id')) && ($picture = Picture::find_by_id ($id, array ('select' => 'id, pv')))))
+      return $this->output_json (array ('status' => false));
+
+    $picture->pv += 1;
+
+    $update = Picture::transaction (function () use ($picture) {
+      return $picture->save ();
+    });
+
+    if (!$update)
+      return $this->output_json (array ('status' => false));
+
+    return $this->output_json (array ('status' => true, 'pv' => $picture->pv));
+  }
 }
