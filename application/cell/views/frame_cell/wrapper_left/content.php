39aba9a739f1d213f3fe38328388ac18c33701d0
OA Wu <comdan66@gmail.com>
2015-10-31 20:15:52 +0800

    Fix layout.

--- application/cell/views/frame_cell/wrapper_left/content.php
+++ application/cell/views/frame_cell/wrapper_left/content.php
@@ -10,8 +10,12 @@
       <h4><?php echo $menus_text;?></h4>
 <?php if ($menus) { ?>
         <div>
-    <?php foreach ($menus as $menu_text => $menu) { ?>
-            <a href='<?php echo $menu['href'];?>'<?php echo ($c = ((isset ($menu['class']) && $menu['class']) && ($class == $menu['class']) && (isset ($menu['method']) && $menu['method']) && ($method == $menu['method'])) || (((isset ($menu['class']) && $menu['class'])) && ($class == $menu['class']) && !((isset ($menu['method']) && $menu['method']))) || (!(isset ($menu['class']) && $menu['class']) && (isset ($menu['method']) && $menu['method']) && ($method == $menu['method'])) ? $menu['icon'] ? $menu['icon'] . ' a' : 'a': $menu['icon']) ? " class='" . $c . "'" : '';?><?php echo $menu['target'] == '_blank' ? 'target="_blank"' : '';?>><?php echo $menu_text;?></a>
+    <?php foreach ($menus as $menu_text => $menu) {
+            if ($menu == 'line') { ?>
+              <a class='l'></a>
+      <?php } else { ?>
+              <a href='<?php echo $menu['href'];?>'<?php echo ($c = ((isset ($menu['class']) && $menu['class']) && ($class == $menu['class']) && (isset ($menu['method']) && $menu['method']) && ($method == $menu['method'])) || (((isset ($menu['class']) && $menu['class'])) && ($class == $menu['class']) && !((isset ($menu['method']) && $menu['method']))) || (!(isset ($menu['class']) && $menu['class']) && (isset ($menu['method']) && $menu['method']) && ($method == $menu['method'])) ? $menu['icon'] ? $menu['icon'] . ' a' : 'a': $menu['icon']) ? " class='" . $c . "'" : '';?><?php echo $menu['target'] == '_blank' ? 'target="_blank"' : '';?>><?php echo $menu_text;?></a>
+      <?php }?>
     <?php } ?>
         </div>
 <?php }
