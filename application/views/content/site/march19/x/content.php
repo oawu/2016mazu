5aecbb95d112a4bc8113bbae1950a4f85977f512
OA Wu <comdan66@gmail.com>
2015-12-17 11:26:15 +0800

    Remove no use file.

--- application/views/content/site/march19/x/content.php
+++
@@ -1,5 +0,0 @@
-
-<div class='map'>
-  <i></i><i></i><i></i><i></i>
-  <div id='map' data-lat='<?php echo $center['lat'];?>' data-lng='<?php echo $center['lng'];?>' data-places_details='<?php echo json_encode (array_slice($places_details, 0));?>'></div>
-</div>
