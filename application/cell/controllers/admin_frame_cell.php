b5f57aa39eca90f4438a3247561e2a4d347029ad
OA Wu <comdan66@gmail.com>
2016-02-24 14:51:28 +0800

    Redesign cell controller.

---
+++ application/cell/controllers/admin_frame_cell.php
@@ -0,0 +1,55 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2016 OA Wu Design
+ */
+
+class Admin_frame_cell extends Cell_Controller {
+
+  /* render_cell ('admin_frame_cell', 'header', var1, ..); */
+  // public function _cache_header () {
+  //   return array ('time' => 60 * 60, 'key' => null);
+  // }
+  public function header ($subtitle = '', $back_link = '') {
+    return $this->load_view (array (
+          'subtitle' => $subtitle,
+          'back_link' => $back_link
+        ));
+  }
+
+  /* render_cell ('admin_frame_cell', 'wrapper_left', var1, ..); */
+  // public function _cache_wrapper_left () {
+  //   return array ('time' => 60 * 60, 'key' => null);
+  // }
+  public function wrapper_left ($menus_list, $class, $metohd, $uri = null) {
+    $class = $class ? $class : $this->CI->router->fetch_class ();
+    $metohd = $metohd ? $metohd : $this->CI->router->fetch_method ();
+    
+    return $this->load_view (array (
+          'menus_list' => $menus_list,
+          'c' => $class,
+          'm' => $metohd,
+          'uri' => $uri,
+        ));
+  }
+
+  /* render_cell ('admin_frame_cell', 'tabs', var1, ..); */
+  // public function _cache_tabs () {
+  //   return array ('time' => 60 * 60, 'key' => null);
+  // }
+  public function tabs ($tabs = array (), $index = null) {
+    return $this->load_view (array (
+        'tabs' => $tabs,
+        'index' => $index
+      ));
+  }
+
+  /* render_cell ('admin_frame_cell', 'footer', var1, ..); */
+  // public function _cache_footer () {
+  //   return array ('time' => 60 * 60, 'key' => null);
+  // }
+  public function footer () {
+    return $this->load_view ();
+  }
+}
--- application/cell/controllers/frame_cell.php
+++ application/cell/controllers/frame_cell.php
@@ -2,61 +2,10 @@
 
 /**
  * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2015 OA Wu Design
+ * @copyright   Copyright (c) 2016 OA Wu Design
  */
 
 class Frame_cell extends Cell_Controller {
-
-  /* render_cell ('frame_cell', 'wrapper_left', var1, ..); */
-  // public function _cache_wrapper_left ($type, $class = null, $method = null) {
-  //   $class = $class ? $class : $this->CI->get_class ();
-  //   $method = $method ? $method : $this->CI->get_method ();
-
-  //   return array ('time' => 60 * 60, 'key' => $type . '_' . $class . '_' . $method);
-  // }
-  public function wrapper_left ($type, $class = null, $method = null) {
-    $class = $class ? $class : $this->CI->get_class ();
-    $method = $method ? $method : $this->CI->get_method ();
-    $menus_list = Cfg::setting ('menu', $type);
-
-    return $this->load_view (array (
-        'menus_list' => $menus_list,
-        'class' => $class,
-        'method' => $method
-      ));
-  }
-
-  /* render_cell ('frame_cell', 'navbar', var1, ..); */
-  // public function _cache_nav ($type, $subtitle = '', $back_link = '') {
-  //   return array ('time' => 60 * 60, 'key' => $type . '_' . $subtitle);
-  // }
-  public function navbar ($type, $subtitle = '', $back_link = '') {
-    return $this->load_view (array (
-        'type' => $type,
-        'subtitle' => $subtitle,
-        'back_link' => $back_link,
-      ));
-  }
-
-  /* render_cell ('frame_cell', 'footer', var1, ..); */
-  // public function footer ($type) {
-  //   return array ('time' => 60 * 60, 'key' => $type);
-  // }
-  public function footer ($type) {
-    return $this->load_view ();
-  }
-
-  /* render_cell ('frame_cell', 'tabs', var1, ..); */
-  // public function tabs ($type, $tabs = array (), $index = null) {
-  //   return array ('time' => 60 * 60, 'key' => $type . '_' . implode ('|', array_keys ($tabs)) . ($index !== null ? '_' . $index : ''));
-  // }
-  public function tabs ($type, $tabs = array (), $index = null) {
-
-    return $this->load_view (array (
-        'tabs' => $tabs,
-        'index' => $index
-      ));
-  }
 
   /* render_cell ('frame_cell', 'pagination', $pagination); */
   // public function _cache_pagination () {
