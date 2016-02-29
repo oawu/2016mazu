045f1a2bbcb9c1e2b2be5ce45f249ec0c17f935a
OA Wu <comdan66@gmail.com>
2016-02-29 21:37:01 +0800

    Redesign role config.

--- application/config/setting/role.php
+++ application/config/setting/role.php
@@ -2,13 +2,17 @@
 
 /**
  * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2015 OA Wu Design
+ * @copyright   Copyright (c) 2016 OA Wu Design
  */
 
-$role = array (
-    'root' => array ('name' => 'root', 'choice' => true),
-    'admin' => array ('name' => '管理員', 'choice' => true),
-    'login' => array ('name' => '登入', 'choice' => false),
-    'guest' => array ('name' => '遊客', 'choice' => false),
+// key 不准亂更改！
+$role['role_names'] = array (
+    'member' => '登入後台',
+    'march19' => '編輯三月十九',
+    'picture' => '管理相簿',
+    'youtube' => '管理影音',
+    'dintao' => '管理藝陣',
+    'root' => '最高權限',
   );
 
+$role['roles'] = array_keys ($role['role_names']);
