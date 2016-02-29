661c4808ef313ce196e7df642d3330e30d95a495
OA Wu <comdan66@gmail.com>
2016-02-29 21:35:53 +0800

    Redesign routes.

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,46 +1,72 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
 Route::root ('main');
+Route::get ('/tag', 'main');
+Route::get ('/login', 'platform@login');
+Route::get ('/platform/index', 'platform@login');
+Route::get ('/platform', 'platform@login');
+
+Route::resourcePagination_site (array (array ('dintaos', 'dintao')), 'dintaos');
+Route::resourcePagination_site (array (array ('pictures', 'picture')), 'pictures');
+Route::resourcePagination_site (array (array ('youtubes', 'youtube')), 'youtubes');
+
+Route::resourcePagination_site (array ('tag', array ('dintaos', 'dintao')), 'tag_dintaos');
+Route::resourcePagination_site (array ('tag', array ('pictures', 'picture')), 'tag_pictures');
+Route::resourcePagination_site (array ('tag', array ('youtubes', 'youtube')), 'tag_youtubes');
 
 Route::group ('admin', function () {
   Route::get ('/', 'main');
+  Route::get ('/tag', 'main');
 
-  Route::resourcePagination (array ('dintao_tags'), 'dintao_tags');
-  Route::resourcePagination (array ('picture_tags'), 'picture_tags');
-  Route::resourcePagination (array ('youtube_tags'), 'youtube_tags');
-  Route::resourcePagination (array ('dintaos'), 'dintaos');
-  Route::resourcePagination (array ('pictures'), 'pictures');
-  Route::resourcePagination (array ('youtubes'), 'youtubes');
-  Route::resourcePagination (array ('dintao_tags', 'dintaos'), 'dintao_tag_dintaos');
-  Route::resourcePagination (array ('picture_tags', 'pictures'), 'picture_tag_pictures');
-  Route::resourcePagination (array ('youtube_tags', 'youtubes'), 'youtube_tag_youtubes');
+  Route::resourcePagination_is_on_site (array ('dintao-tags'), 'dintao_tags');
+  Route::resourcePagination_is_on_site (array ('picture-tags'), 'picture_tags');
+  Route::resourcePagination_is_on_site (array ('youtube-tags'), 'youtube_tags');
 
-  Route::resourcePagination (array ('paths'), 'paths');
-  Route::resourcePagination (array ('paths', 'infos'), 'path_infos');
+  Route::resourcePagination_is_enabled (array ('dintaos'), 'dintaos');
+  Route::resourcePagination_is_enabled (array ('pictures'), 'pictures');
+  Route::resourcePagination_is_enabled (array ('youtubes'), 'youtubes');
+
+  Route::resourcePagination_is_enabled (array ('tag', 'dintaos'), 'tag_dintaos');
+  Route::resourcePagination_is_enabled (array ('tag', 'pictures'), 'tag_pictures');
+  Route::resourcePagination_is_enabled (array ('tag', 'youtubes'), 'tag_youtubes');
 });
 
-$site = array (
-  'dintao' => array ('all' => '所有陣頭', 'keywords' => array ('駕前陣頭', '地方陣頭', '其他介紹')),
-  'picture' => array ('all' => '所有照片', 'keywords' => array ('北港舊照片', '2015三月十九')),
-  'youtube' => array ('all' => '所有影片', 'keywords' => array ('紀錄片')),
-  );
 
-foreach ($site as $key => $values) {
-  foreach ($values['keywords'] as $keyword) {
-    Route::get ($key . 's/' . $keyword . '/', $key . 's@index(' . $keyword . ', 0)');
-    Route::get ($key . 's/' . $keyword . '/(:num)', $key . 's@index(' . $keyword . ', $1)');
-  }
-  Route::get ($key . 's/', $key . 's@all(0)');
-  Route::get ($key . 's/(:num)', $key . 's@all($1)');
-  Route::get ($key . 's/' . $values['all'] . '/', $key . 's@all(0)');
-  Route::get ($key . 's/' . $values['all'] . '/(:num)', $key . 's@all($1)');
+//   Route::resourcePagination (array ('dintao_tags'), 'dintao_tags');
+//   Route::resourcePagination (array ('picture_tags'), 'picture_tags');
+//   Route::resourcePagination (array ('youtube_tags'), 'youtube_tags');
+//   Route::resourcePagination (array ('dintaos'), 'dintaos');
+//   Route::resourcePagination (array ('pictures'), 'pictures');
+//   Route::resourcePagination (array ('youtubes'), 'youtubes');
+  // Route::resourcePagination (array ('dintao_tags', 'dintaos'), 'dintao_tag_dintaos');
+//   Route::resourcePagination (array ('picture_tags', 'pictures'), 'picture_tag_pictures');
+//   Route::resourcePagination (array ('youtube_tags', 'youtubes'), 'youtube_tag_youtubes');
 
-  // Search on
-  Route::get ($key . 's/(:any)/', $key . 's@all(0, $1)');
-  Route::get ($key . 's/(:any)/(:num)', $key . 's@all($2, $1)');
+//   Route::resourcePagination (array ('paths'), 'paths');
+//   Route::resourcePagination (array ('paths', 'infos'), 'path_infos');
+// });
 
-  Route::get ($key . '/(:num)', $key . 's@content(all, $1)');
-  Route::get ($key . '/(:num)-(:any)', $key . 's@content(' . $values['all'] . ', $1)');
-  Route::get ($key . '/(:any)/(:num)', $key . 's@content($1, $2)');
-  Route::get ($key . '/(:any)/(:num)-(:any)', $key . 's@content($1, $2)');
-}
+// $site = array (
+//   'dintao' => array ('all' => '所有陣頭', 'keywords' => array ('駕前陣頭', '地方陣頭', '其他介紹')),
+//   'picture' => array ('all' => '所有照片', 'keywords' => array ('北港舊照片', '2015三月十九')),
+//   'youtube' => array ('all' => '所有影片', 'keywords' => array ('紀錄片')),
+//   );
+
+// foreach ($site as $key => $values) {
+//   foreach ($values['keywords'] as $keyword) {
+//     Route::get ($key . 's/' . $keyword . '/', $key . 's@index(' . $keyword . ', 0)');
+//     Route::get ($key . 's/' . $keyword . '/(:num)', $key . 's@index(' . $keyword . ', $1)');
+//   }
+//   Route::get ($key . 's/', $key . 's@all(0)');
+//   Route::get ($key . 's/(:num)', $key . 's@all($1)');
+//   Route::get ($key . 's/' . $values['all'] . '/', $key . 's@all(0)');
+//   Route::get ($key . 's/' . $values['all'] . '/(:num)', $key . 's@all($1)');
+
+//   // Search on
+//   Route::get ($key . 's/(:any)/', $key . 's@all(0, $1)');
+//   Route::get ($key . 's/(:any)/(:num)', $key . 's@all($2, $1)');
+
+//   Route::get ($key . '/(:num)', $key . 's@content(all, $1)');
+//   Route::get ($key . '/(:num)-(:any)', $key . 's@content(' . $values['all'] . ', $1)');
+//   Route::get ($key . '/(:any)/(:num)', $key . 's@content($1, $2)');
+//   Route::get ($key . '/(:any)/(:num)-(:any)', $key . 's@content($1, $2)');
+// }
