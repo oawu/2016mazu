a303628651f694fae900c51d16c41dd88d406bd0
OA Wu <comdan66@gmail.com>
2016-03-07 23:52:44 +0800

    Add article migrations.

---
+++ application/migrations/022_add_article_tags.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 OA Wu Design
+ */
+
+class Migration_Add_article_tags extends CI_Migration {
+  public function up () {
+    $this->db->query (
+      "CREATE TABLE `article_tags` (
+        `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+        `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '名稱',
+        `is_on_site` tinyint(1) unsigned NOT NULL DEFAULT 1 COMMENT '前台顯示，1 顯示，0 不顯示',
+        `updated_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '更新時間',
+        `created_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '新增時間',
+        PRIMARY KEY (`id`),
+        KEY `name_index` (`name`),
+        KEY `is_on_site_index` (`is_on_site`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
+    );
+  }
+  public function down () {
+    $this->db->query (
+      "DROP TABLE `article_tags`;"
+    );
+  }
+}
---
+++ application/migrations/023_add_article_tag_mappings.php
@@ -0,0 +1,31 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 OA Wu Design
+ */
+
+class Migration_Add_article_tag_mappings extends CI_Migration {
+  public function up () {
+    $this->db->query (
+      "CREATE TABLE `article_tag_mappings` (
+        `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+        `article_id` int(11) unsigned NOT NULL COMMENT 'article ID',
+        `article_tag_id` int(11) unsigned NOT NULL COMMENT 'article Tag ID',
+        `updated_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '更新時間',
+        `created_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '新增時間',
+        PRIMARY KEY (`id`),
+        KEY `article_id_index` (`article_id`),
+        KEY `article_tag_id_index` (`article_tag_id`),
+        UNIQUE KEY `article_id_article_tag_id_unique` (`article_id`, `article_tag_id`),
+        FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE,
+        FOREIGN KEY (`article_tag_id`) REFERENCES `article_tags` (`id`) ON DELETE CASCADE
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
+    );
+  }
+  public function down () {
+    $this->db->query (
+      "DROP TABLE `article_tag_mappings`;"
+    );
+  }
+}
