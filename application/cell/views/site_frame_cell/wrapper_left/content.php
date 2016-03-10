fdc1dc2b483ab62b109860d4420dc2deb34f10ca
OA Wu <comdan66@gmail.com>
2016-03-10 23:43:33 +0800

    Fix code.

--- application/cell/views/site_frame_cell/wrapper_left/content.php
+++ application/cell/views/site_frame_cell/wrapper_left/content.php
@@ -5,6 +5,9 @@
   </div>
 
 <?php
+echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" /><pre>';
+var_dump ($menus_list);
+exit ();
   if ($menus_list) {
     foreach ($menus_list as $menus_text => $menus) { ?>
 <?php if ($menus = array_filter ($menus, function ($menu) { return !(isset ($menu['no_show']) && $menu['no_show']); })) { ?>
