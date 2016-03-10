ababe66bb66108ae4af47262d04a690da808eab6
OA Wu <comdan66@gmail.com>
2016-03-10 23:44:19 +0800

    Fix code.

--- application/core/controllers/03_site_controller.php
+++ application/core/controllers/03_site_controller.php
@@ -31,7 +31,9 @@ class Site_controller extends Oa_controller {
       return redirect_message (array (), array (
           '_flash_message' => ''
         ));
-
+echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" /><pre>';
+var_dump ($menus_list);
+exit ();
     $this->set_componemt_path ('component', 'site')
          ->set_frame_path ('frame', 'site')
          ->set_content_path ('content', 'site')
