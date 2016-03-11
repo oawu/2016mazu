519ea65ebf9ad41bdfb9ca2c86aea4d42ba1a570
OA Wu <comdan66@gmail.com>
2016-03-11 15:38:23 +0800

    Fix code.

--- system/database/drivers/mysql/mysql_driver.php
+++ system/database/drivers/mysql/mysql_driver.php
@@ -88,7 +88,7 @@ class CI_DB_mysql_driver extends CI_DB {
 			$this->hostname .= ':'.$this->port;
 		}
 
-		return @mysqli_pconnect($this->hostname, $this->username, $this->password);
+		return @mysql_pconnect($this->hostname, $this->username, $this->password);
 	}
 
 	// --------------------------------------------------------------------
