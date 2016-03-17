a3704d1cbfd7f5d55de5dc8943a8164ee23400b5
OA Wu <comdan66@gmail.com>
2016-03-18 03:13:21 +0800

    Fix Code.

--- application/controllers/api/baishatun.php
+++ application/controllers/api/baishatun.php
@@ -16,7 +16,7 @@ class Baishatun extends Api_controller {
     else
       header ('Access-Control-Allow-Origin: *');
 
-    $this->version = 24;
+    $this->version = 25;
   }
 
   public function com ($id = 0) {
