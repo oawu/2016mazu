99502e4bdf99b2008207d08e2095b2c3043ae7a1
OA Wu <comdan66@gmail.com>
2016-03-20 12:13:05 +0800

    no message

--- application/controllers/api/baishatun.php
+++ application/controllers/api/baishatun.php
@@ -125,6 +125,56 @@ class Baishatun extends Api_controller {
 
     return @unlink ($path);
   }
+
+  public function mail ($msgs = array ()) {
+    if (!$msgs)
+      $msgs = array (
+          '錯誤原因' => '不明原因錯誤！',
+          '錯誤時間' => date ('Y-m-d H:i:s'),
+        );
+    
+    $html = "<article style='font-size:15px;line-height:22px;color:rgb(85,85,85)'><p style='margin-bottom:0'>Hi 管理員,</p><section style='padding:5px 20px'><p>剛剛發生了系統異常的狀況，以下是錯誤訊息：</p><table style='width:100%;border-collapse:collapse'><tbody>";
+    foreach ($msgs as $title => $msg)
+      $html .= "<tr><th style='width:100px;text-align:right;padding:11px 5px 10px 0;border-bottom:1px dashed rgba(200,200,200,1)'>" . $title . "：</th><td style='text-align:left;text-align:left;padding:11px 0 10px 5px;border-bottom:1px dashed rgba(200,200,200,1)'>" . $msg . "</td></tr>";
+    $html .= "</tbody></table><br/><p style='text-align:right'>如果需要詳細列表，可以置<a href='" . base_url ('admin') . "' style='color:rgba(96,156,255,1);margin:0 2px'>管理後台</a>檢閱。</p></section></article>";
+    
+    $this->load->library ('OaMailGun');
+    $mail = new OaMailGun ();
+    $result = $mail->sendMessage (array (
+              'from' => Cfg::setting ('mail_gun', 'user', 'system', 'name') . ' <' . Cfg::setting ('mail_gun', 'user', 'system', 'email') . '>',
+              'to' => 'OA' . ' <comdan66@gmail.com>',
+              'subject' => '[檢舉信件] ' . Cfg::setting ('mail_gun', 'user', 'system', 'subject'),
+              'html' => $html
+            ));
+  }
+  public function black ($id = 0) {
+    if (!(($id = trim ($id)) && ($msg = BaishatunMessage::find ('one', array ('select' => 'ip', 'conditions' => array ('id = ? AND user_id = ?', $id, 0)))))) {
+      echo 'NO Any Data!';
+      return ;
+    }
+
+    BaishatunBlacklist::create (array (
+        'ip' => $msg->ip,
+      ));
+
+    $this->_put_mag ();
+
+    echo 'OK!';
+  }
+  public function to_black () {
+    if (!(($id = OAInput::post ('id')) && ($id = trim ($id)) && ($msg = BaishatunMessage::find ('one', array ('conditions' => array ('id = ? AND user_id = ?', $id, 0)))))) 
+      return $this->output_json (array ('s' => true));
+    
+    $this->mail (array (
+        'ID' => $msg->id,
+        'IP' => $msg->ip,
+        '內容' => $msg->message,
+        '時間' => $msg->created_at->format ('Y-m-d H:i:s'),
+        '黑名單' => base_url ('api', 'baishatun', 'black', $msg->id),
+      ));
+    
+    return $this->output_json (array ('s' => true));
+  }
   public function ip () {
     if (!(($ip = OAInput::post ('ip')) && ($ip = trim ($ip)))) 
       return $this->output_json (array ('s' => true));
