48fe3e914ce69e6b1588148fd314b76170c24557
OA Wu <comdan66@gmail.com>
2016-03-20 10:11:45 +0800

    Fix Code.

--- application/controllers/cli2.php
+++ application/controllers/cli2.php
@@ -134,7 +134,7 @@ class Cli2 extends Site_controller {
 
     return $qs;
   }
-  public function baishatun ($version = 40) {
+  public function baishatun ($version = 41) {
     $log = CrontabLog::start ('每 1 分鐘更新路線');
     $path = FCPATH . 'temp/api.json';
 
