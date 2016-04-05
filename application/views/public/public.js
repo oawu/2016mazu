1d17c80b4f998e5b06ae13eccf98d8c96681e49f
OA Wu <comdan66@gmail.com>
2016-04-05 23:23:43 +0800

    fix code.

--- application/views/public/public.js
+++ application/views/public/public.js
@@ -3,6 +3,14 @@
  * @copyright   Copyright (c) 2016 OA Wu Design
  */
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-46121102-23', 'auto');
+ga('send', 'pageview');
+
 Array.prototype.column = function (k) {
   return this.map (function (t) { return k ? eval ("t." + k) : t; });
 };
