01e12137cb50a0c64f57c4e213e4d04b58e4b82f
OA Wu <comdan66@gmail.com>
2016-04-06 11:56:57 +0800

    fix code.

--- application/controllers/dintaos.php
+++ application/controllers/dintaos.php
@@ -94,9 +94,9 @@ class Dintaos extends Site_controller {
     return $this->set_title ($title . ' - ' . Cfg::setting ('site', 'title'))
                 ->set_subtitle ($title)
                 ->add_meta (array ('name' => 'keywords', 'content' => implode (',', $tags)))
-                ->add_meta (array ('name' => 'description', 'content' => implode (' ', array_merge (array ($title), column_array ($dintaos, 'title')))))
+                ->add_meta (array ('name' => 'description', 'content' => implode (' ', array_merge (array ($title, Cfg::setting ('site', 'title')), column_array ($dintaos, 'title')))))
                 ->add_meta (array ('property' => 'og:title', 'content' => $title . ' - ' . Cfg::setting ('site', 'title')))
-                ->add_meta (array ('property' => 'og:description', 'content' => implode (' ', array_merge (array ($title), column_array ($dintaos, 'title')))))
+                ->add_meta (array ('property' => 'og:description', 'content' => implode (' ', array_merge (array ($title, Cfg::setting ('site', 'title')), column_array ($dintaos, 'title')))))
                 ->load_view (array (
                     'dintaos' => $dintaos,
                     'pagination' => $pagination,
