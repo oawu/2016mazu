c884e1d8ca0c4c1f9d28bddc6d6f172ba3227add
OA Wu <comdan66@gmail.com>
2016-04-15 18:00:00 +0800

    Fix code!

--- application/controllers/api/march_paths.php
+++ application/controllers/api/march_paths.php
@@ -62,10 +62,10 @@ class March_paths extends Api_controller {
 
   public function create () {
     $paths = ($paths = OAInput::post ('p')) ? $paths : array ();
-
-    $same = is_numeric ($same = OAInput::post ('s')) && $same ? true : false;
+    $same = OAInput::post ('s');
+    // $same = is_numeric ($same = OAInput::post ('s')) && $same ? true : false;
 echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" /><pre>';
-var_dump (is_numeric ($same = OAInput::post ('s')) && $same);
+var_dump ($same ? 1 : 2);
 exit ();
     if (!$paths) return $this->output_json (array ('ids' => array ()));
 
